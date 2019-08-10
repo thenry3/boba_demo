dc9e8507fbebed5c8d2bbfdd97dcb65858d93594
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-08-10 13:51:18 -0700

    Search bar, want to add more to this later

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,55 @@
 import * as React from "react";
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 const NavComponent = styled("div")`
   display: flex;
   position: sticky;
-  background: #30e3a7;
+  background: #06d198;
   justify-content: flex-start;
   align-items: center;
-  font-family: "Lato", sans-serif;
+  font-family: "Comfortaa", sans-serif;
 `;
 
 const WebsiteTitle = styled(Link)`
   color: white;
-  margin: 10px 5vw;
-  margin-right: 0px;
+  margin: 15px 5vw;
+  margin-right: 20px;
   font-size: 3.5vw;
   font-weight: bold;
   letter-spacing: 0.1em;
   text-decoration: none;
+`;
+
+const SearchDiv = styled("div")`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: relative;
+`;
+
+const Searchbar = styled("input")`
+  border: none;
+  position: relative;
+  height: 100%;
+  border-radius: 25px;
+  padding: 6px 20px;
+  padding-right: 12%;
+  font-size: 1.3vw;
+  color: #484848;
+  width: 100%;
+  &:focus {
+    outline: none;
+  }
+  ${SearchDiv}:hover & {
+    color: blue;
+  }
+`;
+
+const SearchIcon = styled(FaSearch)`
+  position: absolute;
+  right: 4%;
 `;
 
 export default class Navbar extends React.Component {
@@ -26,7 +57,11 @@ export default class Navbar extends React.Component {
     return (
       <>
         <NavComponent>
-          <WebsiteTitle to="/">BobaRate</WebsiteTitle>
+          <WebsiteTitle to="/">bobarate</WebsiteTitle>
+          <SearchDiv>
+            <Searchbar placeholder="Search a milk tea shop" />
+            <SearchIcon />
+          </SearchDiv>
         </NavComponent>
       </>
     );
