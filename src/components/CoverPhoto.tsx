621bd85301875fe4d810b6f31cf8eecd48235638
thenry3 <henrymtrinh14@gmail.com>
2019-08-11 10:25:55 -0700

    Initialize Cover photo component

---
+++ src/components/CoverPhoto.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Photo = styled("div")`
+  background: green;
+`;
+
+export default class CoverPhoto extends React.Component {
+  render() {
+    return <></>;
+  }
+}
--- src/pages/Demo_Page.tsx
+++ src/pages/Demo_Page.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 
 import Navbar from "../components/Navbar";
+import CoverPhoto from "../components/CoverPhoto";
 
 export default class Demo_Page extends React.Component {
   render() {
     return (
       <>
         <Navbar />
+        <CoverPhoto />
       </>
     );
   }
